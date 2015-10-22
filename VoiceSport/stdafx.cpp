452b895b67521ad0aaa8799bd8c7c455607ae5e4
truongdq <truongdq54@gmail.com>
2015-10-22 23:10:36 +0900

    Simplify VoiceSports, and make it as main solution

--- VoiceSport/stdafx.cpp
+++ VoiceSport/stdafx.cpp
@@ -1,6 +1,6 @@
 
 // stdafx.cpp : source file that includes just the standard includes
-// VoiceSports.pch will be the pre-compiled header
+// VoiceSport.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
