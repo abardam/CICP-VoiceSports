22b746a4f32adb9efc3e73b071d25436c2f2762e
Enzo Dayrit <enzodayrit11@gmail.com>
2015-11-02 14:33:31 +0900

    Adds TestKinectRead MFC project to test KinectRead

---
+++ TestKinectRead-MFC/TestKinectRead-MFCDlg.h
@@ -0,0 +1,40 @@
+
+// TestKinectRead-MFCDlg.h : header file
+//
+
+#pragma once
+
+
+// CTestKinectReadMFCDlg dialog
+class CTestKinectReadMFCDlg : public CDialog
+{
+// Construction
+public:
+	CTestKinectReadMFCDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TESTKINECTREADMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+public:
+	void ShowKinect();
+
+private:
+	CStatic * m_picture;
+};
