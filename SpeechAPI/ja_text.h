8d6d5a86abee0ff0d445c28e3be6689c78015557
antonio-t <antonio-t>
2016-02-19 19:22:28 +0900

    Japanese feedback corrected

--- SpeechAPI/ja_text.h
+++ SpeechAPI/ja_text.h
@@ -12,20 +12,19 @@
 #define RELBOW_NAME L"右肘"
 #define RHAND_NAME L"右手"
 #define TORSO_NAME L"胴体"
-#define LHIP_NAME L"左股関節"
+#define LHIP_NAME L"左腰"
 #define LKNEE_NAME L"左膝"
 #define LFOOT_NAME L"左足"
-#define RHIP_NAME L"右股関節"
+#define RHIP_NAME L"右腰"
 #define RKNEE_NAME L"右膝"
 #define RFOOT_NAME L"右足"
-#define BACKWARD L"前へ"
-#define FORWARD L"後ろへ"
+#define BACKWARD L"前に"
+#define FORWARD L"後ろに"
 #define TO_THE_LEFT L"左に"
 #define TO_THE_RIGHT L"右に"
-#define UP L"上"
-#define DOWN L"下"
-std::vector<std::wstring> FB_GOOD_POS = { L"あなたは完璧な位置を持っています", L"あなたは素晴らしい仕事をしてきました",
-L"それは最高だ"};
-std::vector<std::wstring> FB_WRONG_BEGIN = { L"あなたの"};
+#define UP L"上に"
+#define DOWN L"下に"
+std::vector<std::wstring> FB_GOOD_POS = { L"正しい姿勢です", L"素晴らしい", L"そこで完璧", L"グッジョブ", L"上手！"};
+std::vector<std::wstring> FB_WRONG_BEGIN = { L""};
 std::vector<std::wstring> FB_WRONG_END = { L"を動かしてください" };
 #endif
